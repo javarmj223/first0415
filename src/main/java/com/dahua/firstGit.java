7168e9f15a5bdbb6f2ecdaa0fb9b8efc06c891b1
lenovo <rmj_java_223@163.com>
2021-04-15 20:22:45 +0800

    newbrouch编辑了这个文件

--- src/main/java/com/dahua/firstGit.java
+++ src/main/java/com/dahua/firstGit.java
@@ -3,5 +3,6 @@ package com.dahua;
 public class firstGit {
     public static void main(String[] args) {
         System.out.println("第一个上传项目");
+        System.out.println("newBrouch写的代码");
     }
 }
